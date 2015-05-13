8b90e8a679e81bc334d710b808a99214c0eed4de
maxim rees <provocatie@msn.com>
2015-05-13 03:20:22 +0200

    First working version, added bugfree signname and image retrieval, added interactive guessing and reguessing

--- app/src/main/java/edu/lb/max/signpractice/MainActivity.java
+++ app/src/main/java/edu/lb/max/signpractice/MainActivity.java
@@ -3,20 +3,23 @@ package edu.lb.max.signpractice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,23 +29,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownServiceException;
 import java.security.SecureRandom;
+import java.sql.Time;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    TextView urlResult;
-    EditText takeUrl;
-    Button Download;
-    ImageView signImage;
+    private TextView signName;
+    private ImageView signImage;
+    private TextView countDown;
+    private  Bitmap signBitmap;
+
     private static final String DEBUG_TAG = "HttpGet";
 
 
@@ -51,64 +53,90 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        urlResult = (TextView) findViewById(R.id.textView);
-        takeUrl = (EditText) findViewById(R.id.editText);
-        Download = (Button) findViewById(R.id.button);
-        Download.setOnClickListener(DownloadButtonListener);
         signImage = (ImageView) findViewById(R.id.signBitmap);
+        signName = (TextView) findViewById(R.id.signName);
+        countDown = (TextView) findViewById(R.id.countDown);
+        ConnectivityManager ConnMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = ConnMgr.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()){
+            initiateGuess();
+        } else {
+            Toast.makeText(getApplicationContext(), "no network connection available.", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    View.OnClickListener DownloadButtonListener = new View.OnClickListener() {
+    private void initiateGuess() {
+        new DownloadTask().execute("32");
+        new CountDownTimer(10000, 100) {
+
+            public void onTick(long millisUntilFinished) {
+                countDown.setText("seconds remaining: " + millisUntilFinished / 1000);
+                if (millisUntilFinished <= 1000) countDown.setText("OMG UR SO SLOW :p x) :')");
+                if (millisUntilFinished <= 900) countDown.setBackgroundColor(Color.RED);
+                if (millisUntilFinished <= 700){
+                    countDown.setBackgroundColor(Color.BLUE);
+                    countDown.setTextColor(Color.YELLOW);
+                }
+                if (millisUntilFinished <= 500) countDown.setBackgroundColor(Color.GREEN);
+                if (millisUntilFinished <= 300) countDown.setBackgroundColor(Color.RED);
+            }
+
+            public void onFinish() {
+                countDown.setText("TIMESUP!!!! AUFSTEIGEN!!!");
+                signImage.setImageBitmap(signBitmap);
+                countDown.setOnClickListener(restartGuessClick);
+            }
+        }.start();
+    }
+
+    View.OnClickListener restartGuessClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            ConnectivityManager ConnMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = ConnMgr.getActiveNetworkInfo();
-            if (netInfo != null && netInfo.isConnected()){
-                new DownloadTask().execute("32");
-            } else {
-                urlResult.setText("no network connection available.");
-            }
+            countDown.setOnClickListener(null);
+            signImage.setImageBitmap(null);
+            initiateGuess();
         }
     };
 
-    // Given a URL, establishes an HttpUrlConnection and retrieves
-    // the web page content as a InputStream, which it returns as
-    // a string.
+
+    // Establishes an HttpUrlConnection and retrieves
+    // a random sign entry whose name and image icon are
+    // set as the next 'display' and 'answer' for the app
     private class DownloadTask extends AsyncTask<String, Void, String> {
         private Bitmap image = null;
         private SecureRandom r = new SecureRandom();
         private String baseUri = "http://gebaren.ugent.be";
         private String allWordsUri = "/alfabet.php?letter=!";
+        private String wordUri = "/alfabet.php?id=";
         @Override
         protected String doInBackground(String... seed) {
+//           generate random sign based on seed, if link invalid IOexception catched and reload doinbackground
             r.setSeed(r.generateSeed(Integer.parseInt(seed[0])));
-            int random = 17090+r.nextInt(22500-17000);
+            int random = 17090+r.nextInt(22400-17000);
+
             // params comes from the execute() call: params[0] is the url.
             try {
-//                generate random sign based on seed, if link invalid IOexception catched and reload doinbackground
-                InputStream myStream = downloadUrl(baseUri+allWordsUri);
-                ArrayList<String> list = parseNameandImageURI(myStream);
+//              get the inputstream of the random words' webpage
+                InputStream myStream = downloadUrl(baseUri+wordUri+random);
+//              get the image and wordname of this word
+                ArrayList<String> list = parseNameAndImageURI(myStream);
 //              return the imagestream by using the image uri on the first arraylist spot
                 myStream = downloadUrl(list.get(0));
                 BitmapFactory bitfac = new BitmapFactory();
                 image = bitfac.decodeStream(myStream);
                 return list.get(1);
-
-            } catch (UnknownServiceException e){
-                Log.d(DEBUG_TAG, "Unknown Service Exception, correct base link wrong request");
-                doInBackground(seed[0]);
-                return "failed1";
             } catch (IOException e) {
-                Log.d(DEBUG_TAG, "IO Exception at downloadUrl");
-                doInBackground(seed[0]);
-                return "failed2";
+                Log.d(DEBUG_TAG, "IO Exception at downloadUrl OR random number failed a page match");
+                return  doInBackground(seed[0]);
+            } catch(ArrayIndexOutOfBoundsException e1){
+                return "failed3";
             }
         }
-        private ArrayList<String> parseNameandImageURI(InputStream myStream) throws IOException {
+        private ArrayList<String> parseNameAndImageURI(InputStream myStream) throws IOException {
 
             try {
                 String contentAsString = readIt(myStream);
-                if (contentAsString.isEmpty()) return new ArrayList<>(0);
+                if (contentAsString.contains("Woord niet gevonden!")) throw new IOException();
                 //parse html and get src and alt of img tag
                 Document doc = Jsoup.parse(contentAsString);
                 Elements bleh = doc.select("img[src*=sign]");
@@ -143,7 +171,7 @@ public class MainActivity extends ActionBarActivity {
         them back together or we could dowload all possible words and store them with a progress bar the first time
         */
         private InputStream downloadUrl(String myUrl) throws IOException, UnknownServiceException {
-            InputStream is = null;
+            InputStream is;
             URL url = new URL(myUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000 /* milliseconds */);
@@ -162,18 +190,10 @@ public class MainActivity extends ActionBarActivity {
         // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
-            urlResult.setText(result);
-            signImage.setImageBitmap(image);
-
+            signName.setText(result);
+            signBitmap = image;
         }
     }
-
-
-
-
-
-
-
 
 
     @Override
